ae5c2794a1381dcfcdb9d43a5309dad5e5a1f806
Vanhnguyencoder <vietanhnguyen0123456789@gmail.com>
2023-10-09 23:14:51 +0700

    ok

--- login.php
+++ login.php
@@ -12,16 +12,17 @@ include('connect.php');
 
 <body>
     <div class="container-xxl position-relative bg-white d-flex p-0">
-        <!-- Spinner Start -->
+        <?php
+            if(isset($_SESSION['dangnhapthanhcong'])) {
+                echo '<meta http-equiv="refresh" content="1;URL=index.php">';
+            } else {
+        ?>
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
             <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
                 <span class="sr-only">Đang tải...</span>
             </div>
         </div>
-        <!-- Spinner End -->
 
-
-        <!-- Sign In Start -->
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
@@ -32,26 +33,56 @@ include('connect.php');
                             </a>
                             <h3>Đăng nhập</h3>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="abcd@gmail.com">
-                            <label for="floatingInput">Địa chỉ Email</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Mật khẩu">
-                            <label for="floatingPassword">Mật khẩu</label>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="rememberme">
-                                <label class="form-check-label" for="rememberme">Ghi nhớ tài khoản</label>
-                            </div>  
-                        </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Đăng nhập</button>
+
+                        <form name="formdangnhap" method="POST" action="login.php">
+                            <div class="form-floating mb-3">
+                                <input name="email" type="email" class="form-control" id="floatingInput" placeholder="abcd@gmail.com">
+                                <label for="floatingInput">Địa chỉ Email</label>
+                            </div>
+                        
+                            <div class="form-floating mb-4">
+                                <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Mật khẩu">
+                                <label for="floatingPassword">Mật khẩu</label>
+                            </div>
+                        
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="rememberme">
+                                    <label class="form-check-label" for="rememberme">Ghi nhớ tài khoản</label>
+                                </div>  
+                            </div>
+                            
+                            <button name="tienhanhdangnhap" type="submit" class="btn btn-primary py-3 w-100 mb-4">Đăng nhập</button>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Sign In End -->
+
+        <?php
+            }
+            if(isset($_POST['tienhanhdangnhap'])) {
+                $email = mysqli_escape_string($connect, $_POST['email']);
+                $password = md5($_POST['password']);
+
+                $sqlcheckuser = "SELECT * FROM `users` WHERE `email` = '". $email ."' AND `password` = '". $password ."'";
+                $ketqua = $connect->query($sqlcheckuser);
+
+                if($ketqua->num_rows > 0) {
+                    echo 'Đăng nhập thành công.';
+                    $user = $ketqua->fetch_array();
+                    $_SESSION['dangnhapthanhcong'] = 'OK';
+                    $_SESSION['username'] = $user["username"];
+                    $_SESSION['email'] = $user["email"];
+                    // header("Refresh: 0; url='/'");
+                    echo '<meta http-equiv="refresh" content="1;URL=index.php">';
+                } else {
+                    echo '<script>alert("Mật khẩu hoặc tên tài khoản không chính xác.");</script>';
+                    // header("Refresh: 0; ");
+                    echo '<meta http-equiv="refresh" content="1;URL=login.php">';
+                }
+            }
+        ?>
     </div>
 
     <?php include('templates/scripts.php') ?>
